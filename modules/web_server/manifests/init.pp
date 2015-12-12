68c87765a436e64e38e9e43f269610b9f0814f23
John Newbigin <jnewbigin@chrysocome.net>
2015-12-13 10:50:56 +1100

    Install the web server

---
+++ modules/web_server/manifests/init.pp
@@ -0,0 +1,28 @@
+
+class web_server::install {
+	package{ "httpd": 	ensure => latest, }
+	package{ "httpd-tools": ensure => latest, }
+}
+
+class web_server::firewall {
+}
+
+class web_server::service {
+        #service { "httpd":
+        #        ensure => running,
+        #        hasstatus => true,
+        #        hasrestart => true,
+        #        enable => true,
+        #        require => Class["web_server::config"],
+        #}
+}
+
+class web_server::config {
+}
+
+class web_server {
+	include web_server::install
+	include web_server::config
+	include web_server::firewall
+	include web_server::service
+}
