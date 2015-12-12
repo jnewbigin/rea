bd87727866d83575f2e5715a201f4c59f733a1cc
John Newbigin <jnewbigin@chrysocome.net>
2015-12-13 10:50:32 +1100

    Prepare the sinatra app for deploy

--- modules/sinatra/manifests/init.pp
+++ modules/sinatra/manifests/init.pp
@@ -1,4 +1,21 @@
 define sinatra::deploy($dir = $name) {
+	include web_server::passenger
+	exec { "bundle $name":
+		cwd => $dir,
+		command => "/usr/bin/bundle install",
+		require => [
+			Package['rubygem-bundler'],
+			Class['web_server::passenger'],
+		],
+		# we can get notified when the dir changes and only build then
+	} ->
+	file { "$dir/public":
+		ensure => directory,
+	} ->
+	file { "$dir/tmp":
+		ensure => directory,
+	}
+	# patch into the web server config
 }
 
 class sinatra {
