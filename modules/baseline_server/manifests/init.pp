d5e2fbfcdfd636d3c914ec86da7b494bee330401
John Newbigin <jnewbigin@chrysocome.net>
2015-12-13 20:33:06 +1100

    Not much to do to the baseline

--- modules/baseline_server/manifests/init.pp
+++ modules/baseline_server/manifests/init.pp
@@ -1,15 +1,10 @@
 class baseline_server {
-	# include puppet
-	#include runlevel
-	#include sysctl
-	#include xinetd
-	#include iptables
-	#include ssh
-	#include ssh_keys
-	#include selinux
-	#include yum
-	#include ipv6
 	print{ "Configuring baseline server": }
+	# CentOS-7 is pretty much locked down out of the box
+	# and there are no site specific requirements here
+	include firewall
+	include yum::update
+	include yum::kernel
 
 }
 
